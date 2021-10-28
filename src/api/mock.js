172b5d1d716b0702a8aa019172459975a7104cc3
ParkDyel <bluetibe@gmail.com>
2021-10-28 12:47:57 +0900

    🧱 product: 상품 목록 렌더링 구현
    - REST API Adaptor를 구현했습니다.
    - REST API MOCK Data를 구현했습니다.
    - 상품 목록 렌더링을 구현했습니다.

---
+++ src/api/mock.js
@@ -0,0 +1,21 @@
+export default {
+  GET: {
+    product: [
+      {
+        id: 1,
+        title: '빨간 장작',
+        path: '/assets/img/product/1.jpg',
+      },
+      {
+        id: 2,
+        title: '도깨비불 장작',
+        path: '/assets/img/product/2.jpg',
+      },
+      {
+        id: 3,
+        title: '비행기',
+        path: '/assets/img/product/3.jpg',
+      },
+    ],
+  },
+};
